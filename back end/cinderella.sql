476081433de29ae1dd641fcd7250424d853f4d2a
Hendra Latieful Maajid <109803213+HendraMaajid@users.noreply.github.com>
2023-11-25 13:41:45 +0700

    Update file fix cinderella.sql

--- back end/cinderella.sql
+++ back end/cinderella.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2023 at 10:49 AM
+-- Generation Time: Nov 25, 2023 at 07:37 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -59,8 +59,13 @@ CREATE TABLE `pengguna` (
 --
 
 INSERT INTO `pengguna` (`username`, `password`, `nama`, `role`) VALUES
+('doriko', '$2y$10$jqmfEVQugB/aRuVkuaSfnuiu0Lr4ksebDN9.re1mW77mJTindWstG', 'doriko', 'putri'),
+('kaito', '$2y$10$3mMw8eT2vqZc5KAbp2lon.cP.avkXMpzXGS5sTLDYQFvy2fmSW4u6', 'Kaito', 'putri'),
+('luka', '$2y$10$aaa0EpnT7fa7J7GgkiUIGOb/9iBOPNEqLz/HX249vBkEH1G/Rk88S', 'Luka', 'putri'),
+('meiko', '$2y$10$7OFF3fQP.Louss58LLczxu4D1X0at91dkC8Txr4bMdnwSed.AO1BC', 'meiko', 'putri'),
 ('miku', '$2y$10$7zK8i7CbuDuGgnYC61pQsu1wktPCueI6CSZMTDlDt8MSTQaiqSHCG', 'Hatsune Miku', 'putri'),
-('pangeran', '$2y$10$FkoM8VdeuE/a6e4ZScNavOabEdpS4ulihj/Xfc6Fkyf/evVZMdruC', 'Charming', 'pangeran'),
+('mikumiku12', '$2y$10$gFLQ7leaR1SicmKETgRI0e8jOD9Xq1wPoKYaYjkLkDs3NVNewFJki', 'Dicky', 'putri'),
+('pangeran', '$2y$10$jqmfEVQugB/aRuVkuaSfnuiu0Lr4ksebDN9.re1mW77mJTindWstG', 'Charming', 'pangeran'),
 ('rin', '$2y$10$ZgWQomjaTWmm3/B6Bk22cePNCymtBzZM5TR3aeObhDpGxJlKnakW6', 'kagamine rin', 'putri');
 
 -- --------------------------------------------------------
@@ -81,8 +86,12 @@ CREATE TABLE `putri` (
 --
 
 INSERT INTO `putri` (`id_putri`, `nama_putri`, `id_sayembara`, `username`) VALUES
-(12, 'Hatsune in', '1', 'miku'),
-(15, 'kagamine rin', 's1', 'rin');
+(12, 'Hatsune Miku', 'A7', 'miku'),
+(15, 'kagamine rin', 's1', 'rin'),
+(28, 'doriko', 'A7', 'doriko'),
+(29, 'meiko', 'A8', 'meiko'),
+(30, 'Luka', 'A8', 'luka'),
+(31, 'Kaito', 'B6', 'kaito');
 
 --
 -- Triggers `putri`
@@ -117,8 +126,11 @@ CREATE TABLE `sayembara` (
 --
 
 INSERT INTO `sayembara` (`id_sayembara`, `daerah`, `status`) VALUES
-('1', 'ajibarang', 'end'),
-('s1', 'jakarta', 'on going');
+('1', 'Luminaria', 'On going'),
+('A7', 'Sunvale', 'On going'),
+('A8', 'Cryshaven', 'End'),
+('B6', 'Tranquil ', 'On going'),
+('s1', 'Starlight', 'End');
 
 -- --------------------------------------------------------
 
@@ -139,7 +151,9 @@ CREATE TABLE `sepatu` (
 INSERT INTO `sepatu` (`id_sepatu`, `size`, `id_putri`) VALUES
 (6, 23, 12),
 (10, 23, 15),
-(12, 37.85, 0);
+(12, 37.85, 0),
+(14, 56, 28),
+(32, 12, 31);
 
 --
 -- Indexes for dumped tables
@@ -193,13 +207,13 @@ ALTER TABLE `pangeran`
 -- AUTO_INCREMENT for table `putri`
 --
 ALTER TABLE `putri`
-  MODIFY `id_putri` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_putri` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `sepatu`
 --
 ALTER TABLE `sepatu`
-  MODIFY `id_sepatu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_sepatu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- Constraints for dumped tables
